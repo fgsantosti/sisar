e5badf0f7ce13b56089317d58bee9c1dc5030464
fgsantosti <fgsantos.ti@gmail.com>
2018-02-23 16:39:55 -0300

    Atualizacao 23 de fevereiro 2018

---
+++ funcoes-sisar.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+import datetime, time
+from datetime import date
+
+# Create your models here.
+
+class Professor(models.Model):
+-inserir - ok
+-listar - ok 
+-alterar - ok
+-excluir -ok
+
+class Modalidade(models.Model):
+-inserir - ok
+-listar - ok 
+-alterar - ok
+-excluir -ok
+
+class Disciplina(models.Model):
+-inserir - ok
+-listar - ok 
+-alterar - ok
+-excluir -ok
+
+class Curso(models.Model):
+-inserir - ok
+-listar - ok 
+-alterar - ok
+-excluir -ok
+
+class Turma(models.Model):
+-inserir - ok
+-listar -  ok
+-alterar - ok
+-excluir -ok
+
+class Antecipacao(models.Model):
+-inserir - off
+-listar - off 
+-alterar - off
+-excluir -off
+
+class Reposicao(models.Model):
+-inserir - off
+-listar - off 
+-alterar - off
+-excluir -off
+
+class Falta(models.Model):
+-inserir - ok
+-listar - ok 
+-alterar - ok
+-excluir -ok
